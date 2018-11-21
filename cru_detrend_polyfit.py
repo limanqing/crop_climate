5aa37eb67480fddce509632f600f133d491692a7
limanqing <42885316+limanqing@users.noreply.github.com>
2018-11-21 15:54:20 +0800

    Update cru_detrend_polyfit.py

--- cru_detrend_polyfit.py
+++ cru_detrend_polyfit.py
@@ -16,23 +16,22 @@ def Polyfit_detrend(x,y):        #主程序，相当于C语言的main函数
     return y_quad
 
 if __name__ == '__main__':
-    base_dir = r'F:\crop-climate\crucsv\*.csv'
+    base_dir = r'F:\crop-climate\cru_scale\360\*.csv'
     filelist = glob.glob(base_dir)
     for filename in filelist:
         df = pd.read_csv(filename)  #用pandas读入数据
         grid_id=filename[-10:-4]
         year_list =  df['Year']    #获取年份列("Year")的数据
         dataframe1=pd.DataFrame({'Year':year_list})
-        dataframe2=pd.DataFrame({'Year':year_list})
-        factor=['Cld','Pre','Tmn','Tmp','Tmx']
+#        dataframe2=pd.DataFrame({'Year':year_list})
+        factor=['Pre','Tmp']
         for f in factor:
             cru_list = df[f]
             if len(set(cru_list))==1:
-                break
+                dataframe1[f]=[0]*116
+                continue
             ys=Polyfit_detrend(year_list, cru_list)
             dataframe1[f]=cru_list-ys
-            dataframe2[f]=cru_list/ys
-        if len(set(cru_list))==1:
-            continue
-        dataframe1.to_csv(r'F:\crop-climate\cru_detrend\polyfit-additive/%s.csv' % (grid_id),index=False)
-        dataframe2.to_csv(r'F:\crop-climate\cru_detrend\polyfit-multiplicative/%s.csv' % (grid_id),index=False)
+#            dataframe2[f]=cru_list/ys
+        dataframe1.to_csv(r'F:\crop-climate\cru_scale_detrend\360\polyfit-additive/%s.csv' % (grid_id),index=False)
+#        dataframe2.to_csv(r'F:\crop-climate\cru_detrend\polyfit-multiplicative/%s.csv' % (grid_id),index=False)
