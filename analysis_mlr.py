20e1395ee64c6a3a81831ed8e5418e924bdb627d
limanqing <42885316+limanqing@users.noreply.github.com>
2018-11-21 15:59:30 +0800

    Update analysis_mlr.py

--- analysis_mlr.py
+++ analysis_mlr.py
@@ -10,24 +10,24 @@ def mlr(y, x):
     res = mod.fit()
     print(res.params)
     s=''
-    for col in range(4):
+    for col in range(3):
         s += '%.3f,' % (res.params[col])
     return s
 
 
 if __name__ == '__main__':
-    datadir = r'F:\crop-climate\maize&cru\linear-additive\*.csv'
+    datadir = r'F:\crop-climate\maize&cru_scale\360\polyfit-additive\*.csv'
     filelist = glob.glob(datadir)
-    result_file = r'F:\crop-climate\regression\mlr\linear-additive.csv'
+    result_file = r'F:\crop-climate\regression_scale\polyfit-additive\360.csv'
     fresult = open(result_file, 'w')
-    cols_result = ['grid','const','cld_coef','pre_coef','tmp_coef']
+    cols_result = ['grid','const','pre_coef','tmp_coef']
     fresult.write(','.join(cols_result) + '\n')#写第一列的标签
 
     for filename in filelist:
         grid_id=filename[-10:-4]
         line = str(grid_id) + ','
         df = pd.read_csv(filename)
-        s = mlr(df['Value'], df[['Cld','Pre','Tmp']])
+        s = mlr(df['Value'], df[['Pre','Tmp']])
         line = line + s 
         fresult.write(line + '\n')
     
